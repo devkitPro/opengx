6b8e3e2b3c2708938951638937cfb4b060feb426
Alberto Mardegan <mardy@users.sourceforge.net>
2024-06-09 16:13:44 +0300

    Add stubs for gl{Push,Pop}ClientAttrib

--- src/gc_gl.c
+++ src/gc_gl.c
@@ -2399,6 +2399,8 @@ void glLightModelf(GLenum pname, GLfloat param) {}
 void glLightModeli(GLenum pname, GLint param) {}
 void glPushAttrib(GLbitfield mask) {}
 void glPopAttrib(void) {}
+void glPushClientAttrib(GLbitfield mask) {}
+void glPopClientAttrib(void) {}
 void glPolygonMode(GLenum face, GLenum mode) {}
 void glReadBuffer(GLenum mode) {}
 void glPixelStorei(GLenum pname, GLint param) {}
